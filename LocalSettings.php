080d0dd3b97867ec7c7581c8be68f0af42bb2d92
Alan Reid <mail@alanreid.com.ar>
2013-04-08 17:28:01 -0300

    deploy

--- LocalSettings.php
+++ LocalSettings.php
@@ -10,6 +10,7 @@
 # Further documentation for configuration settings may be found at:
 # http://www.mediawiki.org/wiki/Manual:Configuration_settings
 
+
 error_reporting(0);
 ini_set('display_errors', 0);
 
