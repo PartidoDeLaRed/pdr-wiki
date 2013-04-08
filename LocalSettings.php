f04b402b740420a2e176f39db7d682c0aa7a84a6
Alan Reid <mail@alanreid.com.ar>
2013-04-08 14:41:27 -0300

    Hide errors

--- LocalSettings.php
+++ LocalSettings.php
@@ -10,6 +10,8 @@
 # Further documentation for configuration settings may be found at:
 # http://www.mediawiki.org/wiki/Manual:Configuration_settings
 
+error_reporting(0);
+ini_set('display_errors', 0);
 
 # Protect against web entry
 if ( !defined( 'MEDIAWIKI' ) ) {
